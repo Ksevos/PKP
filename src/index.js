d4765a873c8eb15261a235144f23a8e6791ca9d8
Xellon <eseviakov@gmail.com>
2018-02-17 11:54:52 +0200

    Three.js added, which will be used to render data

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
+//@ts-check
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Visualization from './Visualization';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Visualization />, document.getElementById('render_window'));
 registerServiceWorker();
