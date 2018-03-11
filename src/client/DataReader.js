62a0d12fdd7df0504158893f681e8a6ce254f0a9
Xellon <eseviakov@gmail.com>
2018-03-11 17:28:59 +0200

    Ispleciau klases asiu pasirinkimui

--- src/client/DataReader.js
+++ src/client/DataReader.js
@@ -2,11 +2,16 @@
 
 import DataObject from './CustomObjects/DataObject';
 import axios from 'axios';
-import DataFormatter from "./DataFormatter";
+import DataFormatter from './DataFormatter';
 
 class DataReader {
     constructor(props) {
         this.fileData = DataObject;
+        this.axes = [];
+        this.xAxis = null;
+        this.yAxis = null;
+        this.zAxis = null;
+        this.scene = null;
     }
 
     /**
@@ -22,17 +27,69 @@ class DataReader {
      * @param {THREE.Scene} scene 
      */
     addDataToScene(scene) {
+        this.scene = scene;
         this._queryForData().then(data=>{
             if(!data)
                 return;
 
-            let dataFormatter = new DataFormatter(data);
-            let dataCloud = dataFormatter.getDataCloud();
+            // Get only axis names
+            this.axes = 
+                data.valueNames.filter(
+                    (name,index) => {
+                        index != data.valueNames.length - 1});
 
-            for(let i = 0; i < dataCloud.length; i++){
-                scene.add(dataCloud[i]);
-            }
+            // Set default axis names based on data
+            this._readDefaultAxes(data);
+
+            this._addToScene(
+                scene, 
+                this.xAxis,
+                this.yAxis,
+                this.zAxis);
         });
+    }
+
+    /**
+     * 
+     * @param {string} xAxis 
+     * @param {string} yAxis 
+     * @param {string} zAxis 
+     */
+    changeAxes(xAxis, yAxis, zAxis){
+        this.xAxis = xAxis;
+        this.yAxis = yAxis;
+        this.zAxis = zAxis;
+
+        this._addToScene(
+            this.scene, 
+            this.xAxis,
+            this.yAxis,
+            this.zAxis);
+    }
+
+    _readDefaultAxes(data){
+        if(!data.valueNames)
+            return;
+        this.xAxis = data.valueNames[0];
+        this.yAxis = data.valueNames[1];
+        this.zAxis = data.valueNames[2];
+    }
+
+    _addToScene(scene, xAxis, yAxis, zAxis){
+        if(!this.fileData)
+            return;
+
+        let dataFormatter = 
+            new DataFormatter(
+                this.fileData,
+                xAxis, 
+                yAxis, 
+                zAxis);
+        let dataCloud = dataFormatter.getDataCloud();
+
+        for(let i = 0; i < dataCloud.length; i++){
+            scene.add(dataCloud[i]);
+        }
     }
 
     /**
@@ -44,13 +101,27 @@ class DataReader {
             .then(response => {
                 const fileData = JSON.parse(JSON.stringify(response.data[0]));
                 if (this.fileData !== fileData
-                    && this._isDataValid(fileData)) {
+                    && this._isDataValid(fileData)) {        
                     this.fileData = fileData;
                     return fileData;
                 }
                 else
                     return null;
             }).catch(error=>console.log(error));
+    }
+
+    /**
+     * @returns {string[]}
+     */
+    getAllAxes(){
+        return this.axes;
+    }
+
+    /**
+     * @param {THREE.Scene} scene 
+     */
+    setTHREEScene(scene){
+        this.scene = scene;
     }
 }
 
