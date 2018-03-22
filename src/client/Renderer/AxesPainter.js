fb80db610a52034e08ad3039e212d0f1f308041c
brassSyntax <leon.karpicz@gmail.com>
2018-03-22 09:43:54 +0200

    refactored AxesPainter, added JSDocs

--- src/client/Renderer/AxesPainter.js
+++ src/client/Renderer/AxesPainter.js
@@ -8,42 +8,60 @@ const MATERIAL_X = new THREE.LineBasicMaterial( {color: '#ff0000'} );
 const MATERIAL_Y = new THREE.LineBasicMaterial( {color: '#00ff00'} );
 const MATERIAL_Z = new THREE.LineBasicMaterial( {color: '#0000ff'} );
 
-class AxesPainter extends THREE.Group {
+export default class AxesPainter extends THREE.Group {
 
-    lines = [];
-    axesHelper;
-
-    size;
-    division;
-    dashes;
-
+    /**
+     * Paints AxesHelper and dashes on top of it, meant to be scaled with GridHelper
+     * @param {number} size Grid size
+     * @param {number} division Grid division
+     * @param {number} dashes Amount of dashes in each grid square
+     */
     constructor(size, division, dashes) {
         super();
 
         this.size = size            || 10;
         this.division = division    || 10;
         this.dashes = dashes        || 3;
-        this.axesHelper = new THREE.AxesHelper(size / 2);
+        this.axesHelper = new THREE.AxesHelper(this.size / 2);
+        this.lines = [];
 
         this._paint();
 
         this.add(this.axesHelper);
+        this.add(...this.lines);
     }
 
+    /**
+     * Scales axes to new grid size
+     * @param {number} size
+     */
     scaleTo(size) {
         size = Math.ceil(size) * 2 + 2;
 
         this.size = size;
         this.division = size;
 
+        this.repaint();
+    }
+
+    /**
+     * Removes previous objects and paints them from scratch
+     */
+    repaint() {
         this.remove(this.axesHelper);
-        this.add(new THREE.AxesHelper(size / 2));
+        this.axesHelper = new THREE.AxesHelper(this.size / 2);
+        this.add(this.axesHelper);
 
         this.remove(...this.lines);
         this.lines.length = 0;
         this._paint();
+        this.add(...this.lines);
     }
 
+    /**
+     * Paints axis dashes scaled with grid size and division
+     * @private
+     */
     _paint() {
         let limit = this.division / 2;
 
@@ -68,9 +86,9 @@ class AxesPainter extends THREE.Group {
                     z: this._scaleDashDistance(i, j)
                 };
 
-                this.lines.push(AxesPainter._createAxisDash(dashX, MATERIAL_X, Axis.X));
-                this.lines.push(AxesPainter._createAxisDash(dashY, MATERIAL_Y, Axis.Y));
-                this.lines.push(AxesPainter._createAxisDash(dashZ, MATERIAL_Z, Axis.Z));
+                this.lines.push(AxesPainter._createAxisDash(dashX, Axis.X));
+                this.lines.push(AxesPainter._createAxisDash(dashY, Axis.Y));
+                this.lines.push(AxesPainter._createAxisDash(dashZ, Axis.Z));
             }
 
             // intermediate (longer) dashes
@@ -93,46 +111,64 @@ class AxesPainter extends THREE.Group {
                     z: this._scaleDashDistance(i + 1, 0)
                 };
 
-                this.lines.push(AxesPainter._createAxisDash(longerDashX, MATERIAL_X, Axis.X));
-                this.lines.push(AxesPainter._createAxisDash(longerDashY, MATERIAL_Y, Axis.Y));
-                this.lines.push(AxesPainter._createAxisDash(longerDashZ, MATERIAL_Z, Axis.Z));
+                this.lines.push(AxesPainter._createAxisDash(longerDashX, Axis.X));
+                this.lines.push(AxesPainter._createAxisDash(longerDashY, Axis.Y));
+                this.lines.push(AxesPainter._createAxisDash(longerDashZ, Axis.Z));
             }
-        }
-
-        for(let i = 0; i < this.lines.length; i++) {
-            this.add(this.lines[i]);
         }
     }
 
-    // returns the point on the axis at which the next dash should be drawn
+    /**
+     * Returns the point on the axis at which the next dash should be drawn
+     * @param {number} gridDistance
+     * @param {number} dashIndex
+     * @returns {number}
+     * @private
+     */
     _scaleDashDistance(gridDistance, dashIndex) {
         const dashSeparation = 1 / (this.dashes + 1);
         return gridDistance / (this.division / this.size) + (this.size / this.division) * dashSeparation * dashIndex;
     }
 
-    // returns dash length scaled to 1 grid square
+    /**
+     * Returns dash length scaled to one grid square
+     * @param {number} length
+     * @returns {number}
+     * @private
+     */
     _scaleDashLength(length) {
         return (this.size / this.division) * length;
     }
 
-    static _createAxisDash(centerPoint, material, alignment) {
-        // rewrite this to BufferyGeometry if needed later
+    /**
+     * Creates a new dash on axis point
+     * @param {Object} centerPoint Point on the axis
+     * @param {number} alignment Used to align dashes with Enum.Axis
+     * @returns {Line}
+     * @private
+     */
+    static _createAxisDash(centerPoint, alignment) {
+        // rewrite this to BufferGeometry if needed later
         let geometry = new THREE.Geometry();
+        let material;
 
         switch (alignment) {
             case Axis.X:
                 geometry.vertices.push(new THREE.Vector3( centerPoint.x, centerPoint.y, centerPoint.z ));
                 geometry.vertices.push(new THREE.Vector3( centerPoint.x, centerPoint.y,-centerPoint.z ));
+                material = MATERIAL_X;
                 break;
 
             case Axis.Y:
                 geometry.vertices.push(new THREE.Vector3( centerPoint.x, centerPoint.y, centerPoint.z ));
                 geometry.vertices.push(new THREE.Vector3(-centerPoint.x, centerPoint.y, centerPoint.z ));
+                material = MATERIAL_Y;
                 break;
 
             case Axis.Z:
                 geometry.vertices.push(new THREE.Vector3( centerPoint.x, centerPoint.y, centerPoint.z ));
                 geometry.vertices.push(new THREE.Vector3(-centerPoint.x, centerPoint.y, centerPoint.z ));
+                material = MATERIAL_Z;
                 break;
 
             default:
@@ -142,5 +178,3 @@ class AxesPainter extends THREE.Group {
         return new THREE.Line(geometry,material);
     }
 }
-
-export default AxesPainter;
