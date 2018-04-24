2bcfd12b337567e5c90e6624743f23c774e3de98
Xellon <eseviakov@gmail.com>
2018-04-24 18:05:44 +0300

    Added tooltip

--- src/client/ReactComponents/PointSelection/Button.jsx
+++ src/client/ReactComponents/PointSelection/Button.jsx
@@ -2,25 +2,50 @@
 import React, {Component} from 'react';
 import Enum from '../../CustomObjects/Enum'
 import './Button.css';
+import Tooltip from '../Tooltip';
 
 class PointSelectionButton extends Component{
     constructor(props){
         super(props);
-        this.state = {toggled: false};
+        this.state = {toggled: false, showTooltip: false};
+
+
+
     }
+
+    componentDidMount(){
+        document.getElementById("pointSelectionButton")
+            .addEventListener(
+                "mouseover",
+                event=>{
+                    this.setState({showTooltip: true})
+                });
+        document.getElementById("pointSelectionButton")
+            .addEventListener(
+                "mouseout",
+                event=>{
+                    this.setState({showTooltip: false})
+                });
+    }
+
     render(){
         return(
-            <div>
-                <button className = "PointSelectionButton" 
-                        style = {{
-                            backgroundColor: this.state.toggled ? Enum.Theme.BUTTON_TOGGLED : Enum.Theme.BUTTON_DEFAULT,
-                            borderColor: this.state.toggled ? Enum.Theme.BUTTON_TOGGLED : Enum.Theme.BUTTON_DEFAULT
-                        }}
-                        onClick = { event => {
-                            this.setState(prevState => { return {toggled: !prevState.toggled}});
-                            this.props.onClick(!this.state.toggled);
-                        }}
-                />
+            <div id="pointSelectionButton" >
+                <button 
+                    className = "PointSelectionButton" 
+                    style = {{
+                        backgroundColor: this.state.toggled ? Enum.Theme.BUTTON_TOGGLED : Enum.Theme.BUTTON_DEFAULT,
+                        borderColor: this.state.toggled ? Enum.Theme.BUTTON_TOGGLED : Enum.Theme.BUTTON_DEFAULT
+                    }}
+                    onClick = { event => {
+                        this.setState(prevState => { return {toggled: !prevState.toggled}});
+                        this.props.onClick(!this.state.toggled);
+                    }}/>
+                {this.state.showTooltip 
+                    ? <Tooltip position = {{x:180, y:-100}}
+                        title = "Point Selection Button"
+                        description = "When toggled, hover on a point to get it's index in the data array"/> 
+                    : null}
             </div>
         );
     }
