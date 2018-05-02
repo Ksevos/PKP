3c3021d59b465883ee0ee65166f869c8e336ef42
Xellon <eseviakov@gmail.com>
2018-05-02 19:02:48 +0300

    small fix

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ if (!dev) {
     app.use(express.static(path.resolve(__dirname, 'build')));
 
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+        res.sendFile(path.resolve(__dirname, 'build-server', 'index.html'));
     });
 }
 
